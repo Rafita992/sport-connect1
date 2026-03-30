6c060caacbba35f91107a9d5bec0d165f7f172b1
Rafa <rafamorenomenaya@gmail.com>
2026-03-30 14:25:40 +0200

    Finalizado el login fxml y el controller

---
+++ src/main/java/com/example/sportconnect1/controller/LoginController.java
@@ -0,0 +1,94 @@
+package com.example.sportconnect1.controller;
+
+import com.example.sportconnect1.models.User;
+import com.example.sportconnect1.service.UserService;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class LoginController {
+    @FXML
+    private TextField txtEmail;
+    @FXML
+    private PasswordField txtPassword;
+    @FXML
+    private Button btnLogin;
+    @FXML
+    private Button btnRegister;
+
+    private final UserService userService = new UserService();
+
+    @FXML
+    private void handleLogin(){
+        String email = txtEmail.getText().trim();
+        String password = txtPassword.getText().trim();
+
+        if(email.isEmpty() || password.isEmpty()){
+            System.out.println("Por favor rellena todos los campos");
+        }
+        try {
+            User user = userService.login(email, password);
+
+            if(user != null){
+                if(user.getAdmin()){
+                    navigateToDashboard(user);
+                } else{
+                    navigateToHomeUser(user);
+                }
+            } else{
+                System.out.println("El correo o contraseña han sido incorrectos");
+            }
+        } catch (Exception e) {
+            System.out.println("Error, no se pudo conectar con el servidor");
+            e.printStackTrace();
+        }
+    }
+
+    private void navigateToDashboard(User user){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/sportconnect1/fxml/dashboard.fxml"));
+            Scene scene = new Scene(loader.load());
+            DashboardController controller = loader.getController();
+            //controller.setUser(user);
+            Stage stage = (Stage) btnLogin.getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void navigateToHomeUser(User user){
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/sportconnect1/fxml/home-user.fxml"));
+            Scene scene = new Scene(loader.load());
+            UserHomeController controller = loader.getController();
+            //controller.setUser(user);
+            Stage stage = (Stage) btnLogin.getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void handleRegister(){
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/sportconnect1/fxml/register.fxml"));
+            Scene scene = new Scene(loader.load());
+            RegisterController controller = loader.getController();
+            Stage stage = (Stage) btnLogin.getScene().getWindow();
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
---
+++ src/main/java/com/example/sportconnect1/controller/UserHomeController.java
@@ -0,0 +1,4 @@
+package com.example.sportconnect1.controller;
+
+public class UserHomeController {
+}
